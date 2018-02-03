6483be1f47ff206fa5c75403ae43bafe9c541f2c
xlfdll <xlfdll@gmail.com>
2018-02-03 04:47:59 -0800

    Version 2.0: Reverted property syntax to be compatible with Visual Studio 2015, and changed .NET Framework target version back to 4.6.2 for easy deployments in Windows 7

--- Simon.NET/Objects/SimonPattern.cs
+++ Simon.NET/Objects/SimonPattern.cs
@@ -2,26 +2,26 @@
 
 namespace Simon.NET
 {
-	internal class SimonPattern
-	{
-		internal SimonPattern()
-		{
-			_random = new Random();
-			_pattern = new Int32[100];
+    internal class SimonPattern
+    {
+        internal SimonPattern()
+        {
+            _random = new Random();
+            _pattern = new Int32[100];
 
-			for (Int32 i = 0; i < _pattern.Length; i++)
-			{
-				_pattern[i] = _random.Next(0, 4);
-			}
-		}
+            for (Int32 i = 0; i < _pattern.Length; i++)
+            {
+                _pattern[i] = _random.Next(0, 4);
+            }
+        }
 
-		private Random _random;
-		private Int32[] _pattern;
+        private Random _random;
+        private Int32[] _pattern;
 
-		internal Int32 this[Int32 index]
-		{
-			get => _pattern[index];
-			set => _pattern[index] = value;
-		}
-	}
+        internal Int32 this[Int32 index]
+        {
+            get { return _pattern[index]; }
+            set { _pattern[index] = value; }
+        }
+    }
 }
